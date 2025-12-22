7993f5504aa0df2ce07acc1963f834bb9f0bc684
Romi Kusuma Bakti <romikusumab@gmail.com>
2025-12-23 01:12:19 +0700

    fix: update postgres volume path in compose.yaml and import relations in db index

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
+import { relations } from "./relations";
 
 const client = postgres(process.env.DATABASE_URL!, {
   max: 1,
@@ -9,4 +10,5 @@ const client = postgres(process.env.DATABASE_URL!, {
 export const db = drizzle({
   client,
   schema,
+  relations,
 });
