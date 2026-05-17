8d3547bf9760790c7cce74a56bb0daa099266eff
Romi Kusuma Bakti <romikusumab@gmail.com>
2026-05-17 19:11:53 +0700

    Remove TypeScript build error ignoring from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,6 @@ const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default withNextIntl(nextConfig);
