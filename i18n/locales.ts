1d600ca70fdbccc1d23e4085814938eb4d12dec6
Romi Kusuma Bakti <romikusumab@gmail.com>
2026-05-24 16:16:38 +0700

    Sort locales alphabetically

--- i18n/locales.ts
+++ i18n/locales.ts
@@ -1,4 +1,4 @@
-export const locales = ["id", "en", "zh", "ar"] as const;
+export const locales = ["ar", "en", "id", "zh"] as const;
 export type Locale = (typeof locales)[number];
 
 export const defaultLocale: Locale = "en";
